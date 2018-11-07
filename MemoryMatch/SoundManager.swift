d342785f5c219089f4597fff170b24aa6b43fa94
Lucas Dahl <LucasDahl425@yahoo.com>
2018-11-07 07:26:39 -0800

    Added sound assist and sound manager class work has started.

---
+++ MemoryMatch/SoundManager.swift
@@ -0,0 +1,31 @@
+//
+//  SoundManager.swift
+//  MemoryMatch
+//
+//  Created by Lucas Dahl on 11/7/18.
+//  Copyright © 2018 Lucas Dahl. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class SoundManager {
+    
+    // Properties
+    var audioPlayer:AVAudioPlayer?
+    
+    enum SoundEffect {
+        
+        case flip
+        case shuffle
+        case match
+        case nomatch
+        
+    }
+    
+    // This will play the sounds
+    func playSound() {
+        
+    }
+    
+}
